b098b9591011cd95c2b4aa2aa0886d84288bf565
Manu Kapolke <manukapolke@gmail.com>
2025-08-13 19:30:39 +0200

    Change images

--- src/pages/Concerts/Concerts.tsx
+++ src/pages/Concerts/Concerts.tsx
@@ -1,29 +1,59 @@
+import React from "react";
+import { motion } from "motion/react";
 import { Card } from "../../components/Card/Card";
 import { Typewriter } from "../../components/Typewriter/Typewriter";
 import { UpcomingShow } from "../../components/UpcomingShow/UpcomingShow";
 import { concerts } from "./concertData";
 import styles from "./Concerts.module.less";
+import { zoomInVariants } from "../Home/Home";
+import ÜnpV1Img from "../../assets/übernextparty_v1.svg";
 
 export const Concerts = () => {
-  return (
-    <Card>
-      <Typewriter text="2025" />
-      <div className={styles.upcomingShows}>
-        {concerts
-          .filter((concert) => concert.date.endsWith("2025"))
-          .map((concert) => (
-            <UpcomingShow key={concert.date} concert={concert} />
-          ))}
-      </div>
+  const [imgLoaded, setImgLoaded] = React.useState(false);
 
-      <Typewriter text="2026" />
-      <div className={styles.upcomingShows}>
-        {concerts
-          .filter((concert) => concert.date.endsWith("2026"))
-          .map((concert) => (
-            <UpcomingShow key={concert.date} concert={concert} />
-          ))}
+  React.useEffect(() => {
+    const img = new window.Image();
+    img.src = ÜnpV1Img;
+    img.onload = () => setImgLoaded(true);
+  }, []);
+
+  if (!imgLoaded) {
+    return null; // Oder ein Spinner/Loader
+  }
+
+  return (
+    <div className={styles.contentWrapper}>
+      <div className={styles.imageWrapper}>
+        <motion.img
+          src={ÜnpV1Img}
+          alt="Übernext Party Writing"
+          className="filter"
+          variants={zoomInVariants}
+          initial="small"
+          animate="normal"
+        />
+        {/* <motion.img src={PaperplaneIcon} alt="Paper Plane" className="filter"
+                variants={fadeInVariants} initial="hidden" animate="visible" /> */}
       </div>
-    </Card>
+      <Card style={{ width: "100%" }}>
+        <Typewriter text="2025" />
+        <div className={styles.upcomingShows}>
+          {concerts
+            .filter((concert) => concert.date.endsWith("2025"))
+            .map((concert) => (
+              <UpcomingShow key={concert.date} concert={concert} />
+            ))}
+        </div>
+
+        <Typewriter text="2026" />
+        <div className={styles.upcomingShows}>
+          {concerts
+            .filter((concert) => concert.date.endsWith("2026"))
+            .map((concert) => (
+              <UpcomingShow key={concert.date} concert={concert} />
+            ))}
+        </div>
+      </Card>
+    </div>
   );
 };
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -2,45 +2,75 @@ import { motion } from "motion/react";
 import { Card } from "../../components/Card/Card";
 import { Typewriter } from "../../components/Typewriter/Typewriter";
 import styles from "./Contact.module.less";
-import { zoomInVariants } from "../Home/Home";
-import ÜnpV1Img from '../../assets/übernextparty_v1.svg';
-import FacebookIcon from '../../assets/icons/facebook.svg';
-import MailIcon from '../../assets/icons/email.svg';
-import AtIcon from '../../assets/icons/at-symbol.svg';
+// import { zoomInVariants } from "../Home/Home";
+import { fadeInVariants } from "../Home/Home";
+// import ÜnpV1Img from '../../assets/übernextparty_v1.svg';
+import FacebookIcon from "../../assets/icons/facebook.svg";
+import MailIcon from "../../assets/icons/email.svg";
+import AtIcon from "../../assets/icons/at-symbol.svg";
 import React from "react";
-// import PaperplaneIcon from '../../assets/papierflieger.svg';
-
+import PaperplaneLightIcon from "../../assets/papierflieger-hell.svg";
 
 export const Contact = () => {
-    const [imgLoaded, setImgLoaded] = React.useState(false);
+  const [imgLoaded, setImgLoaded] = React.useState(false);
 
-    React.useEffect(() => {
-        const img = new window.Image();
-        img.src = ÜnpV1Img;
-        img.onload = () => setImgLoaded(true);
-    }, []);
+  React.useEffect(() => {
+    const img = new window.Image();
+    img.src = PaperplaneLightIcon;
+    img.onload = () => setImgLoaded(true);
+  }, []);
 
-    if (!imgLoaded) {
-        return null; // Oder ein Spinner/Loader
-    }
+  if (!imgLoaded) {
+    return null; // Oder ein Spinner/Loader
+  }
 
-    return <div className={styles.contentWrapper}>
-        <div className={styles.imageWrapper}>
-            <motion.img src={ÜnpV1Img} alt="Übernext Party Writing" className="filter"
-                variants={zoomInVariants} initial="small" animate="normal" />
-            {/* <motion.img src={PaperplaneIcon} alt="Paper Plane" className="filter"
-                variants={fadeInVariants} initial="hidden" animate="visible" /> */}
+  return (
+    <div className={styles.contentWrapper}>
+      <div className={styles.imageWrapper}>
+        {/* <motion.img src={ÜnpV1Img} alt="Übernext Party Writing" className="filter"
+                variants={zoomInVariants} initial="small" animate="normal" /> */}
+        <motion.img
+          src={PaperplaneLightIcon}
+          alt="Paper Plane"
+          //   className="filter"
+          variants={fadeInVariants}
+          initial="hidden"
+          animate="visible"
+        />
+      </div>
+      <Card>
+        <div className={styles.textWrapper}>
+          <Typewriter text="Kontakt" />
+          <p>
+            Für Anfragen, Buchungen und weitere Informationen stehen wir gerne
+            zur Verfügung.
+          </p>
+          <div className={styles.contactGrid}>
+            <span className={styles.contactCategory}>
+              <img src={AtIcon} /> Email:
+            </span>{" "}
+            <a href="mailto:info@rockpommelsband.com">
+              info@rockpommelsband.com
+            </a>
+            <span className={styles.contactCategory}>
+              <img src={FacebookIcon} /> Facebook:
+            </span>{" "}
+            <a href="https://facebook.com/rockpommelsband" target="_blank">
+              facebook.com/rockpommelsband
+            </a>
+            <span className={styles.contactCategory}>
+              <img src={MailIcon} /> Post:
+            </span>{" "}
+            <p>
+              Rockpommel's Band
+              <br />
+              Hülsberg 37
+              <br />
+              42349 Wuppertal
+            </p>
+          </div>
         </div>
-        <Card>
-            <div className={styles.textWrapper}>
-                <Typewriter text="Kontakt" />
-                <p>Für Anfragen, Buchungen und weitere Informationen stehen wir gerne zur Verfügung.</p>
-                <div className={styles.contactGrid}>
-                    <span className={styles.contactCategory}><img src={AtIcon} /> Email:</span> <a href='mailto:info@rockpommelsband.com'>info@rockpommelsband.com</a>
-                    <span className={styles.contactCategory}><img src={FacebookIcon} /> Facebook:</span> <a href='https://facebook.com/rockpommelsband' target="_blank">facebook.com/rockpommelsband</a>
-                    <span className={styles.contactCategory}><img src={MailIcon} /> Post:</span> <p>Rockpommel's Band<br />Hülsberg 37<br />42349 Wuppertal</p>
-                </div>
-            </div>
-        </Card>
-    </div>;
+      </Card>
+    </div>
+  );
 };
